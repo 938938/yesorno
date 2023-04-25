6b09cc492e3c86d4dc7f24bc24c4b0d1ce6957e9
938938 <938kjh@gmail.com>
2023-04-26 00:07:02 +0900

    💄[Refactor]: gif 필터 생성, 대기 과정 추가

--- src/store/dataSlice.js
+++ src/store/dataSlice.js
@@ -1,5 +1,10 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
+import LOADING from '../images/loadingCat.png';
+
+const NG = [
+  'https://yesno.wtf/assets/no/3-80a6f5b5d6684674bcfeda34accca4e1.gif',
+];
 
 const dataSlice = createSlice({
   name: 'dataSlice',
@@ -7,13 +12,25 @@ const dataSlice = createSlice({
   extraReducers: (builder) => {
     builder.addCase(asyncAPI.pending, (state, action) => {
       // 대기상태일 때 state 상태
-      state.answer = '';
-      state.image = '';
+      state.answer = '답변을 불러오는 중입니다.';
+      state.image = LOADING;
     });
     builder.addCase(asyncAPI.fulfilled, (state, action) => {
+      if (NG.includes(action.payload.image)) {
+        if (action.payload.answer === 'yes') {
+          action.payload.image =
+            'https://yesno.wtf/assets/yes/10-271c872c91cd72c1e38e72d2f8eda676.gif';
+        } else {
+          action.payload.image =
+            'https://yesno.wtf/assets/no/26-34b31d1f0777f70c61488f67a36576a9.gif';
+        }
+      }
       state.answer = action.payload.answer;
       state.image = action.payload.image;
     });
+    // builder.addCase(asyncAPI.rejected, (state, action) => {
+    //   state.answer = '답변을 받아오는 것에 실패했습니다.';
+    // });
   },
 });
 

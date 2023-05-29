cca23ea53f486d1c7e2a69f30922573c6b141f8b
938938 <938kjh@gmail.com>
2023-05-29 23:13:06 +0900

    fix: 이미지 오류 해결

--- src/components/AnswerContainer.jsx
+++ src/components/AnswerContainer.jsx
@@ -1,26 +1,35 @@
 import * as S from './AnswerContainer.style';
 import { useSelector } from 'react-redux';
 import Answer from './Answer';
+import Loader from './Loader';
 
-const AnswerBox = () => {
-  const answer = useSelector((state) => state.data.answer);
+import CATPOT from '../images/cat-pot.png';
+import CATBACK from '../images/cat-back.png';
+import PUZZLED from '../images/puzzled.png';
+
+const AnswerContainer = () => {
+  const answer = useSelector((state) => state.answer.answer);
   return (
-    <S.AnswerBoxDiv>
+    <S.AnswerContainer>
       <S.Question>
-        <S.Puzzled alt='궁금해하는 이모티콘. 답변칸의 상단에 위치.' />
+        <img src={PUZZLED} alt='궁금해하는 이모티콘. 답변칸의 상단에 위치.' />
       </S.Question>
       <S.Cat>
         {answer ? (
-          <S.CatPot alt='항아리를 안은 고양이 이미지, 답변이 왔을 때 표시' />
+          <img
+            src={CATPOT}
+            alt='항아리를 안은 고양이 이미지, 답변이 왔을 때 표시'
+          />
         ) : (
-          <S.CatBack alt='뒤로 돌아 앉은 고양이 이미지, 질문이 없을 때 혹은 답변이 도착하지 않았을 때 표시' />
+          <img
+            src={CATBACK}
+            alt='뒤로 돌아 앉은 고양이 이미지, 질문이 없을 때 혹은 답변이 도착하지 않았을 때 표시'
+          />
         )}
       </S.Cat>
-      <S.AnswerCard>
-        <Answer />
-      </S.AnswerCard>
-    </S.AnswerBoxDiv>
+      <S.AnswerCard>{answer ? <Answer /> : <Loader />}</S.AnswerCard>
+    </S.AnswerContainer>
   );
 };
 
-export default AnswerBox;
+export default AnswerContainer;
